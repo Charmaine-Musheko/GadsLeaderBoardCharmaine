f22601983ebbef955317bdb087b6d2dc61c38e08
Charmaine <chamainemush@gmail.com>
2020-09-11 20:10:23 +0200

    1

--- app/src/main/java/com/example/gadsleaderboardcharmaine/MainActivity.java
+++ app/src/main/java/com/example/gadsleaderboardcharmaine/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.gadsleaderboardcharmaine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.google.android.material.tabs.TabLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import org.w3c.dom.Comment;
 
@@ -25,12 +35,18 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tablayout;
     private ViewPager viewPager;
     private ViewPagerAdapter adapter;
+    private ImageView images;
+    private ProgressBar mProgressBar;
+    public static int SPLASH_TIME_OUT;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         tablayout = (TabLayout) findViewById(R.id.tablayout_id);
         viewPager = (ViewPager) findViewById(R.id.viewpager_id);
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -39,9 +55,33 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new SkillIq(), getString(R.string.top_iq_score));
         viewPager.setAdapter(adapter);
         tablayout.setupWithViewPager(viewPager);
+        images = (ImageView) findViewById(R.id.imageViewa);
+        images.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent intent = new Intent(MainActivity.this, Post.class);
+                startActivity(intent);
+            }
+        });
+    new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(homeIntent);
+            finish();
 
-
-
+        }
+    },SPLASH_TIME_OUT);
+        onPreExecute();
     }
+
+    protected void onPreExecute() {
+        mProgressBar = (android.widget.ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBar.setProgress(1);
+    }
+
 }
+
+
--- app/src/main/java/com/example/gadsleaderboardcharmaine/Post.java
+++ app/src/main/java/com/example/gadsleaderboardcharmaine/Post.java
@@ -2,36 +2,47 @@ package com.example.gadsleaderboardcharmaine;
 
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
 import retrofit2.Call;
+
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import okhttp3.ResponseBody;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class Post extends AppCompatActivity {
+public class Post extends AppCompatActivity implements View.OnClickListener {
     private EditText editTextEmail;
     private EditText editTextName;
     private EditText editTextLastName;
     private EditText editTextLinkToProject;
+    private ImageView images;
 
+    LayoutInflater layoutInflater;
+
+    @NonNull
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.section_four);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.section_four);
 
-        editTextEmail = findViewById(R.id.edit_email);
-        editTextName = findViewById(R.id.edit_name);
-        editTextLastName = findViewById(R.id.edit_lastname);
-        editTextLinkToProject-= findViewById(R.id.link_to_project);
 
-        findViewById(R.id.).setOnClickListener(this);
-        findViewById(R.id.).setOnClickListener(this);
+            editTextEmail = findViewById(R.id.edit_email);
+            editTextName = findViewById(R.id.edit_name);
+            editTextLastName = findViewById(R.id.edit_lastname);
+            editTextLinkToProject = findViewById(R.id.link_to_project);
+            images = (ImageView) findViewById(R.id.imageViewa);
+            findViewById(R.id.submission_button).setOnClickListener((View.OnClickListener) this);
+
+            layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
 
     }
@@ -48,43 +59,54 @@ public class Post extends AppCompatActivity {
             return;
 
         }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) ;
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
         {
+
             editTextEmail.setError("Enter a valid email");
             editTextEmail.requestFocus();
             return;
         }
 
-        if (Name.isEmpty()) {
-            editTextName.setError("Name is required");
-            editTextName.requestFocus();
-            return;
-        }
 
-        if (LastName.isEmpty()) {
-            editTextLastName.setError("Last Name is required");
-            editTextLastName.requestFocus();
-            return;
 
-        }
-        Call<ResponseBody> call = ApiClient.
-                getInstance().
-                getApi()
-                .formResponse(email, Name, LastName, LinkToProject);
-        call.enqueue((new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                Toast.makeText(Post.this, "Sucessful", Toast.LENGTH_SHORT).show();
+            if (Name.isEmpty()) {
+                editTextName.setError("Name is required");
+                editTextName.requestFocus();
+                return;
             }
 
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(Post.this, "Failed", Toast.LENGTH_LONG).show();
+            if (LastName.isEmpty()) {
+                editTextLastName.setError("Last Name is required");
+                editTextLastName.requestFocus();
+                return;
+
             }
-        }));
-    }
+            Call<ResponseBody> call = ApiClient.
+                    getInstance().
+                    getApi()
+                    .formResponse(email, Name, LastName, LinkToProject);
+            call.enqueue((new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    Toast.makeText(Post.this, "Sucessful", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    Toast.makeText(Post.this, "Failed", Toast.LENGTH_LONG).show();
+                }
+            }));
+        }
 
     @Override
-    public void onClick(View v){
+    public void onClick(View view) {
+switch (view.getId()){
+    case R.id.imageViewa:
+        userSubmit();
+        break;
+    case R.id.submission_button:
+        break;
+}
     }
 }
+
